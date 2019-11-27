82514c3502e8ee7f03d2f4b874420324670d8c1d
Adolfo Valdivieso <adoval4@gmail.com>
2019-11-27 11:38:23 -0300

    login form validation

--- frontend/src/pages/Login.vue
+++ frontend/src/pages/Login.vue
@@ -8,9 +8,10 @@
       <md-app-content >
         <form
           class="md-layout full-height md-alignment-center-center"
+          :class="{ 'md-invalid': !isFormValid }"
           @submit.prevent="validateData"
         >
-          <md-card class="md-layout-item md-size-50 md-small-size-100 ">
+          <md-card class="md-layout-item md-size-30 md-small-size-100 ">
             <md-progress-bar md-mode="indeterminate" v-if="sending" />
 
             <md-card-header>
@@ -19,19 +20,44 @@
             <md-card-content>
               <md-field >
                 <label for="email">Email</label>
-                <md-input type="email" name="email" id="email" autocomplete="email" v-model="form.email" :disabled="sending" />
+                <md-input
+                  type="email"
+                  name="email"
+                  id="email"
+                  :required="true"
+                  autocomplete="email"
+                  v-model="form.email"
+                  :disabled="sending"
+                />
+                <span class="md-error " v-if="form.errors.email.invalid">
+                  Email provided is invalid
+                </span>
               </md-field>
               <md-field >
                 <label for="email">Password</label>
-                <md-input type="password" name="email" id="email" autocomplete="email" v-model="form.password" :disabled="sending" />
+                <md-input
+                  type="password"
+                  name="password"
+                  id="password"
+                  :required="true"
+                  autocomplete="password"
+                  v-model="form.password"
+                  :disabled="sending"
+                />
               </md-field>
-              <span class="md-error " v-if="authError">The email or password provided are incorrect</span>
+              <span class="md-error " v-if="authError">
+                The email or password provided are incorrect
+              </span>
             </md-card-content>
 
-
-
             <md-card-actions>
-              <md-button type="submit" class="md-raised md-primary" :disabled="sending">Get me in!</md-button>
+              <md-button
+                type="submit"
+                class="md-raised md-primary"
+                :disabled="sending"
+              >
+                Get me in!
+              </md-button>
             </md-card-actions>
           </md-card>
 
@@ -41,20 +67,6 @@
   </div>
 </template>
 
-<style lang="scss" scoped>
-
-  $red-accent: #ff5252;
-
-  .md-app {
-    min-height: 100vh;
-    height: 100%;
-  }
-
-  .md-error {
-    color: $red-accent;
-  }
-</style>
-
 <script>
 export default {
   name: 'Login',
@@ -62,21 +74,78 @@ export default {
     return {
       form: {
         email: null,
-        password: null
+        password: null,
+        errors: {
+          email: {
+            empty: false,
+            invalid: false,
+          },
+          password: {
+            empty: false,
+          },
+        }
       },
+      isFormValid: true,
       sending: false,
       authError: false
     }
   },
   methods: {
     validateData() {
+      if(!this.validateForm()) {
+        return;
+      }
+
       this.sending = true;
-      console.log(this.form)
       setTimeout(() => {
         this.authError = true;
         this.sending = false;
       }, 2000)
+    },
+    validateForm() {
+      let isValid = true;
+      this.isFormValid = isValid;
+      // reset errors
+      this.$set(this.form, 'errors', {
+        email: {
+          empty: false,
+          invalid: false,
+        },
+        password: {
+          empty: false,
+        },
+      });
+
+      if(!this.form.email) {
+        this.$set(this.form.errors.email, 'empty', true);
+        isValid = false;
+      }
+      else if(!this.isValidEmail()) {
+        this.$set(this.form.errors.email, 'invalid', true);
+        isValid = false;
+      }
+
+      if(!this.form.password) {
+        this.$set(this.form.errors.password, 'empty', true);
+        isValid = false;
+      }
+
+      this.isFormValid = isValid;
+      return isValid;
+    },
+    isValidEmail: function () {
+      var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(this.form.email);
     }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.md-field .md-error {
+    display: block;
+    left: 0;
+    opacity: 1;
+    transform: translate3d(0, 0px, 0);
+}
+</style>
