db4f1fdbfbc65f08c5a04a9cf974505246b839de
Adolfo Valdivieso <adoval4@gmail.com>
2019-12-07 21:37:47 -0300

    back out of vue router history mode

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -14,7 +14,7 @@ Vue.use(VueMaterial)
 import VueRouter from 'vue-router'
 import routes from './routes'
 Vue.use(VueRouter)
-const router = new VueRouter({ routes, mode: 'history' })
+const router = new VueRouter({ routes })
 
 new Vue({
   el: '#app',
