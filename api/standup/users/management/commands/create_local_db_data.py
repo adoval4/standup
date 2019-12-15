c97ee4d5f8efc141aa48281c78ba35f76f574c6e
Adolfo Valdivieso <adoval4@gmail.com>
2019-12-14 21:36:07 -0300

    command to create super user an initial team

---
+++ api/standup/users/management/commands/create_local_db_data.py
@@ -0,0 +1,28 @@
+# django
+from django.core.management.base import BaseCommand, CommandError
+
+# models
+from standup.users.models import User
+from standup.teams.models import Team
+
+
+class Command(BaseCommand):
+    help = 'Closes the specified poll for voting'
+
+    def handle(self, *args, **options):
+        # create super user
+        email = input('Email: ')
+        first_name = input('First name: ')
+        last_name = input('Last name: ')
+        password = input('Password: ')
+        user = User.objects.create(
+            email=email,
+            first_name=first_name,
+            last_name=last_name,
+        )
+        user.set_password(password)
+        user.save()
+
+        team_name = input('Team\'s name: ')
+        team = Team.objects.create(name=team_name)
+        team.managers.add(user)
